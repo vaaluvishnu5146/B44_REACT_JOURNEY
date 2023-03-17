d6a0ed4118441fc093c824f40c61d929b5149ed3
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2023-03-17 11:19:13 +0530

    First component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./components/card";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="pricing py-5">
+        <div className="container">
+          <div className="row">
+            <Card price="10" />
+            <Card price="20" />
+            <Card price="30" />
+            <Card price="0" />
+            <Card price="100" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/card.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+// REACT COMPONENT
+export default function Card(props) {
+  const { price } = props;
+  console.log(props);
+  return (
+    <div className="col-lg-4">
+      <div className="card mb-5 mb-lg-0">
+        <div className="card-body">
+          <h5 className="card-title text-muted text-uppercase text-center">
+            Free
+          </h5>
+          <h6 className="card-price text-center">
+            ${price}
+            <span className="period">/month</span>
+          </h6>
+          <hr />
+          <ul className="fa-ul">
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              Single User
+            </li>
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              5GB Storage
+            </li>
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              Unlimited Public Projects
+            </li>
+            <li>
+              <span className="fa-li">
+                <i className="fas fa-check"></i>
+              </span>
+              Community Access
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              Unlimited Private Projects
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              Dedicated Phone Support
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              Free Subdomain
+            </li>
+            <li className="text-muted">
+              <span className="fa-li">
+                <i className="fas fa-times"></i>
+              </span>
+              Monthly Status Reports
+            </li>
+          </ul>
+          <div className="d-grid">
+            <a href="#" className="btn btn-primary text-uppercase">
+              Button
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
